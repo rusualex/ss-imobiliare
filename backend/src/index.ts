a87c13f315126ba8444e74c2c5f5c53e6d782fa4
rusualex <rusu_alex_geo@yahoo.com>
2020-04-05 22:45:03 +0300

    Apartments Vertical working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,17 +10,17 @@ import { ResponseWrapperService } from './services/response-wrapper.service';
 import { ApartmentService } from './services/apartment.service';
 import { UserService } from './services/user.service';
 
-const parentRouter: ParentRouter = new ParentRouter();
+const parentRouter: ParentRouter = new ParentRouter(); 
 const userRouter: UserRouter = new UserRouter();
 const apartmentRouter: ApartmentRouter = new ApartmentRouter();
 const loginRouter: LoginRouter = new LoginRouter();
 const userService: UserService = new UserService();
-const apartmentService: ApartmentService = new ApartmentService();
+const apartmentService: ApartmentService = new ApartmentService(); 
 const responseWrapperService: ResponseWrapperService = new ResponseWrapperService();
 const hashService: HashService = new HashService();
 const loginService: LoginService = new LoginService();
 const app: App = new App();
-
+ 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
